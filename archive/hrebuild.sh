2f017b53fbeea0b672196adfa724a3c024ccacea
Person <localboxcrox@gmail.com>
2025-01-20 01:08:53 -0500

    stuff

--- archive/hrebuild.sh
+++ archive/hrebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./push.sh
+./archive/push.sh
 ssh -p 8878 hans@localhost "sudo nixos-rebuild switch && sudo reboot"
---
+++ archive/push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rsync -v --rsync-path="sudo rsync" -e "ssh -p 8878 -o 'StrictHostKeyChecking no'" -r ../nixos/* hans@localhost:/etc/nixos/
+# scp -P 8878 -r -o "StrictHostKeyChecking no" nixos/* root@localhost:/etc/nixos/
