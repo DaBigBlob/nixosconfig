25b69e5fe2f8ba3e32e717ea4f6ad59959563e6c
Person <localboxcrox@gmail.com>
2025-01-21 07:47:11 -0500

    stuff

--- nixos/mod/network.nix
+++ nixos/mod/network.nix
@@ -4,11 +4,55 @@
 {
     networking = {
         networkmanager.enable = true;
+        enableIPv6 = true;
+        useDHCP = false;
+
         firewall = {
             enable = true;
-            allowedTCPPorts = [ 22 ];
+            allowedTCPPorts = [];
             allowedUDPPorts = [];
         };
+
+        # DNS encrypt
+        nameservers = [ "127.0.0.1" "::1" ];
+        networkmanager.dns = "none";
+        dhcpcd = {
+            enable = false;
+            extraConfig = "nohook resolv.conf";
+        };
+
+    };
+
+    # DNS encrypt
+    services.resolved.enable = false;
+    services.dnscrypt-proxy2 = {
+        enable = true;
+        settings = {
+            listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+            ipv4_servers = true;
+            ipv6_servers = true;
+            dnscrypt_servers = true;
+            doh_servers = true;
+            odoh_servers = true;
+            require_dnssec = false;
+            require_nolog = true;
+            require_nofilter = true;
+
+            sources.public-resolvers = {
+                urls = [
+                "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+                "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+                ];
+                cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+                minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+            };
+
+        };
+    };
+
+    # DNS encrypt
+    systemd.services.dnscrypt-proxy2.serviceConfig = {
+        StateDirectory = "dnscrypt-proxy";
     };
 
     services = {
