e5cefb413679f6a7b9d89feaa187ca33330c2dc2
Person <localboxcrox@gmail.com>
2025-01-30 05:59:43 -0500

    stuff

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,7 @@
     description = "thin flake shim for NixOS module purity";
     # so that can be easily replaced with ./archive/configuration.nix as flakes are experimental
 
-    inputs.nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-24.11&narHash=sha256-BPHmb3fUwdHkonHyHi1+x89eXB3kA1jffIpwPVJIVys=";
+    inputs.nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-24.11";
 
     outputs = { nixpkgs, self, ... }:
 
