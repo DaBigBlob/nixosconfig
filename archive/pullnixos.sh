8793f32a543b16605d61c451a88b5e894d7a153b
Person <localboxcrox@gmail.com>
2025-03-02 12:13:58 -0500

    stuff

--- archive/pullnixos.sh
+++ archive/pullnixos.sh
@@ -2,24 +2,33 @@
 
 check_progs() {
     for prg in "$@"; do
+        printf "Checking if %s available... " "$prg"
         if ! command -v "$prg" >/dev/null 2>&1; then
-            printf "%s not in path or not installed\n" "$prg"
+            printf "NO: not in path or not installed\n"
             exit 1
         fi
+        printf "YES\n"
     done
 }
 
 check_progs printf exit git cd cp
 
+printf "Checking if NixOS... "
 if [ ! -d "/etc/nixos/" ]; then
-    printf "this aint NixOS\n"
+    printf "NO\n"
     exit 1
 fi
+printf "YES\n"
 
+printf "Navigating to TMP directory... "
 cd "$(mktemp -d)" || exit 1
+printf "DONE\n"
 
+printf "Git subroutine... \n"
 git clone "https://github.com/DaBigBlob/nixosconfig.git" .
 
+printf "Copying configs to target... "
 cp -r ./nixos/* /etc/nixos/
+printf "DONE\n"
 
 exit 0
