d897677774398f72c91f98a28199328cba825eb4
Person <localboxcrox@gmail.com>
2025-03-02 11:22:02 -0500

    stuff

--- archive/pullnixos.sh
+++ archive/pullnixos.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# rsync -v --rsync-path="sudo rsync" -e "ssh -p 8878 -o 'StrictHostKeyChecking no'" hans@localhost:/etc/nixos/flake.lock ./nixos/flake.lock
-# scp -P 8878 -r -o "StrictHostKeyChecking no" hans@localhost:/etc/nixos/flake.lock ./nixos/flake.lock
-
 if [ ! -d "/etc/nixos/" ]; then
     printf "this aint NixOS\n"
     exit 1
