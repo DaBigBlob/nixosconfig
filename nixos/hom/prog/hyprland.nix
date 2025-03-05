59d514a8d16aa5e944a0b31af4fb2172d572dc6d
Person <localboxcrox@gmail.com>
2025-03-05 10:20:58 -0500

    nixos config

--- nixos/hom/prog/hyprland.nix
+++ nixos/hom/prog/hyprland.nix
@@ -1,5 +1,5 @@
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.kitty.enable = true; # required for the default Hyprland config
@@ -27,6 +27,33 @@
           )
           9)
       );
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
   };
 
   wayland.windowManager.hyprland.plugins = [
