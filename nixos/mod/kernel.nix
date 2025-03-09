2ab8ad2ae4eaf1f67eb9bc26efd4206a38de241d
Person <localboxcrox@gmail.com>
2025-03-09 17:23:39 -0400

    nixos config

--- nixos/mod/kernel.nix
+++ nixos/mod/kernel.nix
@@ -3,7 +3,7 @@
 
 {
     boot = {
-        kernelPackages = pkgs.unstable.linuxKernel.packages.linux_6_13;
+        kernelPackages = pkgs.linuxKernel.packages.linux_6_12_hardened;
         loader = {
             systemd-boot.enable = true;
             efi.canTouchEfiVariables = true;
