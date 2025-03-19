da223c3f47d7e0f10690b1e96187f7d5eb3d0331
Person <localboxcrox@gmail.com>
2025-03-19 14:16:10 -0400

    stuff

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,40 +3,10 @@
   description = "thin flake shim for NixOS module purity";
   # so that can be easily replaced with ./archive/configuration.nix as flakes are experimental
 
-  inputs.fpkgs = {
-    url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-24.11&rev=5ef6c425980847c78a80d759abc476e941a9bf42";
-  };
-
-  outputs = { fpkgs, self, ... }:
-
+  outputs = { self, ... }:
   let
-    # nixpkgs =  (import (import ./utils/channels.nix).nixpkgs) {
-    #   system = (
-    #     (import ./hardware-configuration.nix) {
-    #       config = null;
-    #       lib.mkDefault = a: a;
-    #       pkgs = null;
-    #       modulesPath = null;
-    #     }
-    #   ).nixpkgs.hostPlatform;
-    # };
-    # ppkgs = (import "${(import ./srcs/flakes.nix).nixpkgs}") {
-    #   system = (
-    #     (import ./hardware-configuration.nix) {
-    #       config = null;
-    #       lib.mkDefault = a: a;
-    #       pkgs = null;
-    #       modulesPath = null;
-    #     }
-    #   ).nixpkgs.hostPlatform;
-    # } // (import ./srcs/flakes.nix).nixpkgs;
-    # (import ./srcs/channels.nix).nixpkgs;
     nixSys = file: (import ./pkgs).lib.nixosSystem {
-      specialArgs = {
-        # pkgs = (import ./pkgs);
-        # lib = pkgs.lib;
-        top_flake = self;
-      };
+      specialArgs = { top_flake = self; };
       modules = [
         file
         ./mod
