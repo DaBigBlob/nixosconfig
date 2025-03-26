94e1d5b14b3e50e9c1ffeea737498b609d7ef71a
Person <localboxcrox@gmail.com>
2025-03-26 09:25:54 -0400

    stuff

--- cryx/default.nix
+++ cryx/default.nix
@@ -12,14 +12,19 @@ rec {
   attr_list_to_attr = attr_mutation: attr_list: builtins.foldl' (acc: set: acc // (attr_mutation set)) {} attr_list;
   unname_named_attr = attr: attr_list_to_attr (a: a) (builtins.attrValues attr);
   files_to_attr = post_import: files: attr_list_to_attr (a: a.value) (files_to_attr_list post_import files);
+  himport = args: files: files_to_attr (a: a args) files;
 
-  dem2 = files_to_named_attr (a: a) [
-    ./demo.nix
-    ./demo2.nix
-  ];
-  dem3 = unname_named_attr dem2;
-  dem4 = files_to_attr (a: a) [
-    ./demo.nix
-    ./demo2.nix
+  # dem2 = files_to_named_attr (a: a) [
+  #   ./demo.nix
+  #   ./demo2.nix
+  # ];
+  # dem3 = unname_named_attr dem2;
+  # dem4 = files_to_attr (a: a) [
+  #   ./demo.nix
+  #   ./demo2.nix
+  # ];
+  dem5 = himport 4 [
+    ./hdem1.nix
+    ./hdem2.nix
   ];
 }
