cb2b3ed80cb7720fc01cb246fac7b08ed14c0b7b
Person <localboxcrox@gmail.com>
2025-03-27 13:22:54 -0400

    stuff

--- cryx/cry1.nix
+++ cryx/cry1.nix
@@ -23,13 +23,13 @@ rec {
 
     # deep_attr_merge = attr1: attr2: null;
 
-    # attr_list_to_attr = attr_mutation: attr_list: builtins.foldl' (
-    #   acc: set: acc // (attr_mutation set)
-    # ) {} attr_list;
+    attr_list_to_attr = attr_mutation: attr_list: builtins.foldl' (
+      acc: set: acc // (attr_mutation set)
+    ) {} attr_list;
 
     # unname_named_attr = attr: attr_list_to_attr (a: a) (builtins.attrValues attr);
 
-    # files_to_attr = post_import: files: attr_list_to_attr (a: a.value) (files_to_named_attr_list post_import files);
+    files_to_attr = post_import: files: attr_list_to_attr (a: a.value) (files_to_named_attr_list post_import files);
 
     # himport = args: files: files_to_attr (a: a args) files;
   };
