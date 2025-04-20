612645d8ea9bbf63d0dc65da779027dff5e73190
Person <localboxcrox@gmail.com>
2025-04-19 22:00:52 -0400

    stuff

--- cryx/cry3small.nix
+++ cryx/cry3small.nix
@@ -2,6 +2,8 @@
 rec {
 # hutil is 882 bytes
 hutil = rec{attr_list_to_attr=attr_list: builtins.zipAttrsWith(name: values: if(builtins.length values)==1 then(builtins.head values)else if(builtins.all builtins.isAttrs values)then(attr_list_to_attr values)else if(builtins.all builtins.isList values)then(builtins.concatLists values)else builtins.throw"hutil: Cannot merge: [${builtins.toString values}]")attr_list;files_to_attr_list=post_import: files: builtins.map(file: builtins.listToAttrs[{name=builtins.head(builtins.match"([[:alnum:]_]+).nix"(builtins.baseNameOf file));value=post_import(builtins.import file);}])files;himport_mut=post_import: files: attr_list_to_attr(builtins.map(file: post_import(builtins.import file))files);fimport_mut=post_import: files: attr_list_to_attr(files_to_attr_list post_import files);himport=args: files: himport_mut(fn: fn args)files;fimport=args: files: fimport_mut(fn: fn args)files;};
+
+
 dem8 = hutil.fimport 3 [
     ./hdem1.nix
     ./hdem2.nix
